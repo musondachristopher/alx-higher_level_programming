79126f3b1f8b6ca9bba1ef283195eb802adaf4a2
muzzzo <musondachristopher@rocketmail.com>
2023-08-22 08:25:49 -0700

    0-main.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# By - Musonda Christopher
+# A function that prints all integers of a list
+def print_list_integer(my_list=[]):
+    for py in range(len(my_list)):
+        print("{:d}".format(my_list[py]))
