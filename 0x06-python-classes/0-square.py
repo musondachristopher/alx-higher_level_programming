5178fbfea27536349bb042a7c45874ba1c7d9e3b
muzzzo <musondachristopher@rocketmail.com>
2023-08-30 14:57:20 +0000

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# By - Musonda Christopher
+# A Python empty class Square that defines a square
+"""Define a class Square."""
+class Square:
+    """Represent a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# By - Musonda Christopher
+# A Python class Square that defines a square by: (based on 0-square.py).
+"""Define a class Square."""
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
