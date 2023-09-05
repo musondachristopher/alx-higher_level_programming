9bdb00dd9d685d7bd98eec73746a481453dc58fa
muzzzo <musondachristopher@rocketmail.com>
2023-09-05 20:43:26 +0000

    9-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# By - Musonda Christopher
+# An empty Python class Rectangle that defines a rectangle
+"""class defines a an empty Rectangle class."""
+class Rectangle:
+    """Represents a rectangle function method space"""
+    pass
