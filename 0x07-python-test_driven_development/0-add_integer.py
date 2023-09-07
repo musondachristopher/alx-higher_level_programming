22eba7f11391ac83296b82242ec7b85eb666b64c
muzzzo <musondachristopher@rocketmail.com>
2023-09-07 23:09:21 +0000

    Lesa Wamaka

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # By - Musonda Christopher
-
+# A Python function that adds 2 integers
 """
 This is the "0-add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b).
