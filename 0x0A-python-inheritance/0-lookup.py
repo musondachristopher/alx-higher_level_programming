27bd33df76bc0c1da588d6c8fe030fe7ad76f7f8
muzzzo <musondachristopher@rocketmail.com>
2023-09-14 10:45:13 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # By - Musonda Christopher
 # A Python function that returns the list of available attributes and methods of an object.
-"""Defines an object attribute lookup function."""
+"""Defines an  object attribute lookup function."""
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
