7daec34979799dd42fb329d08ed35c1dce6addf4
muzzzo <musondachristopher@rocketmail.com>
2023-09-18 15:09:03 -0700

    popopo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # By - Musonda Christopher
 # A Python function that reads a text file (UTF8) and prints it to stdout.
-"""Defines a text file-reading function."""
+
+
+"""this defines a text file-reading function."""
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
