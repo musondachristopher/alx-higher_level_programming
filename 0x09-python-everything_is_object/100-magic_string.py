d41b16091fbe36a0288fe41687ce85d3d502e9c4
muzzzo <musondachristopher@rocketmail.com>
2023-09-26 11:07:24 -0700

    everything_is_object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def magic_string(List_of_str=[]):
+     List_of_str += ["BestSchool"]
+
+    return (", ".join(List_of_str))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This is 101-locked_class task
+"""
+
+
+class LockedClass:
+    """ stated a class of locked class """
+    #  define the available slots that can be created
+    __slots__ = ('first_name')
