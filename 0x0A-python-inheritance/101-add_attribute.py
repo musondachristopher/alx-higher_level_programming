028b5485ae941e4904da6da1333fe191d25c1d07
muzzzo <musondachristopher@rocketmail.com>
2023-09-26 13:20:40 -0700

    inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,17 +2,16 @@
 
 """
 File: 101-add_attribute.py
-Desc: This module contains a single function defination
-Author: Gizachew Bayness (Elec Crazy)
-Date Created: Aug 1 2022
+Desc: This module  contains a function defination
+Date Created: sept 2023
 """
 
 
 def add_attribute(obj, att, value):
     """
-    This function adds a new attribute to an object if it’s possible.
+    a funct adds a new attribute to object if it’s possible.
     """
     if hasattr(obj, "__dict__"):
         setattr(obj, att, value)
     else:
-        raise TypeError("can't add new attribute")
+        raise TypeError("can't add niew attribute")
